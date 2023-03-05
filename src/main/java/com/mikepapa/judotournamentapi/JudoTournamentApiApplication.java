959dde2dab99975fd596f49157701ac916b42554
mike <mike@example.com>
2023-03-05 13:42:42 +0100

    Initial commit

---
+++ src/main/java/com/mikepapa/judotournamentapi/JudoTournamentApiApplication.java
@@ -0,0 +1,13 @@
+package com.mikepapa.judotournamentapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JudoTournamentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JudoTournamentApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mikepapa/judotournamentapi/JudoTournamentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikepapa.judotournamentapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JudoTournamentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
